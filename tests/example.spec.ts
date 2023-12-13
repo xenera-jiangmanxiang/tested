0e2fa0592810dbb9b0ad1c51a95b1b18d08f35c0
manxiang0516 <manxiang.jiang@xenera.jp>
2023-12-13 16:46:22 +0800

    adjust codes

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -7,7 +7,27 @@ test('has title', async ({ page }) => {
   await expect(page).toHaveTitle(/Playwright/);
 });
 
-test('get started link', async ({ page }) => {
+test('get started link1', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Get started' }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole('heading', { name: 'Installa2312312313tion' })).toBeVisible();
+});
+
+test('get started link2', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Get started' }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole('heading', { name: 'Installa2312312313tion' })).toBeVisible();
+});
+
+test('get started link3', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Click the get started link.
