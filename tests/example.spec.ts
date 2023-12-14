66541ccc3628f7ffd0790f61fd6d5b086132de2a
manxiang0516 <manxiang.jiang@xenera.jp>
2023-12-14 18:07:52 +0800

    adjust codes

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,11 +1,5 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
 
 test('get started link1', async ({ page }) => {
   await page.goto('https://playwright.dev/');
@@ -14,7 +8,7 @@ test('get started link1', async ({ page }) => {
   await page.getByRole('link', { name: 'Get started' }).click();
 
   // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installa2312312313tion' })).toBeVisible();
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
 
 test('get started link2', async ({ page }) => {
@@ -24,7 +18,7 @@ test('get started link2', async ({ page }) => {
   await page.getByRole('link', { name: 'Get started' }).click();
 
   // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installa2312312313tion' })).toBeVisible();
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
 
 test('get started link3', async ({ page }) => {
@@ -35,4 +29,5 @@ test('get started link3', async ({ page }) => {
 
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+
 });
